0088a3a968f36dbaff351efbd91594c572291a8c
stevendifeo <bxing.day@gmail.com>
2024-10-10 23:31:00 -0400

    added transaction model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Transaction(models.Model):
+    type = models.CharField()
+    exchange = models.CharField()
+    ticker = models.CharField()
+    full_name = models.CharField()
+    date = models.DateField()
+    price = models.IntegerField()
 
