0856ab2cd41ee1858c10b132e368bb6fdc2c0bcb
stevendifeo <bxing.day@gmail.com>
2024-10-16 21:46:03 -0400

    old ui

--- client/src/components/dashboard/PortfolioDetails.tsx
+++ client/src/components/dashboard/PortfolioDetails.tsx
@@ -2,18 +2,29 @@ import { PortfolioActivityEnum } from '@/enums/PortfolioActivityEnum'
 import React, { useState } from 'react'
 import Holdings from './holdings/Holdings'
 import Transactions from './transaction/Transactions'
+import PortfolioTypeBtn from './PortfolioTypeBtn'
 import IUserData from '@/interfaces/IUserData'
 
 interface props {
-    userData: IUserData,
-    portfolioActivityType: PortfolioActivityEnum
+    userData: IUserData
 }
 
-const PortfolioDetails: React.FC<props> = ({ userData, portfolioActivityType }) => {
+const PortfolioDetails: React.FC<props> = ({ userData }) => {
+    const [portfolioActivityType, setPortfolioActivityType] = useState<PortfolioActivityEnum>(PortfolioActivityEnum.HOLDINGS)
+    console.log(userData)
+
+    const createPortfolioTypeButtons = () => {
+        return (Object.keys(PortfolioActivityEnum) as Array<keyof typeof PortfolioActivityEnum>).map((key, index) => (
+            <PortfolioTypeBtn key={index} buttonName={key} setPortfolioActivityType={() => setPortfolioActivityType(PortfolioActivityEnum[key])} />
+        ));
+    };
 
     return (
         <>
             <div className='flex flex-col'>
+                <div>
+                    {createPortfolioTypeButtons()}
+                </div>
 
                 {portfolioActivityType == PortfolioActivityEnum.HOLDINGS && userData !== undefined ?
                     <Holdings userData={userData.user_holdings}></Holdings>
