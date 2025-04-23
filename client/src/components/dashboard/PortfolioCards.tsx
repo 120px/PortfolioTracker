ced5759883265757ac912b5c64e30dbc83b54624
120px <s.difeo2@gmail.com>
2025-04-22 20:51:41 -0400

    temporary fix for total_contrib

--- client/src/components/dashboard/PortfolioCards.tsx
+++ client/src/components/dashboard/PortfolioCards.tsx
@@ -13,10 +13,10 @@ const PortfolioCards: React.FC<props> = ({ userData, userPortfolioValue }) => {
         <div className='w-full flex flex-row gap-5'>
             <Card className='w-1/4' >
                 <CardHeader className='p-6 flex flex-row items-center justify-between space-y-0 pb-2'>Total contribution</CardHeader>
-                <CardContent className='text-2xl font-bold'>{
+                {/* <CardContent className='text-2xl font-bold'>{
                     userData.total_contribution !== undefined ? `$ ${userData.total_contribution}` : null}
                     <p className='text-xs text-muted-foreground font-normal'>Contributed X this month</p>
-                </CardContent>
+                </CardContent> */}
             </Card>
             <Card className='w-1/4' >
                 <CardHeader className='p-6 flex flex-row items-center justify-between space-y-0 pb-2'>Portfolio Value</CardHeader>
